8c773d69097961b730cb0e1c2728fea18c5ebf44
Jeroen de Bruijn <vv9239@gmail.com>
2024-09-25 12:08:42 +0200

    fix: remove undesired `work_in_progress` function from the `git` plugin

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -1,3 +1,8 @@
 for function ("${ZSH_CUSTOM}/functions/"*.zsh); do
   source "${function}"
 done
+
+# The `work_in_progress` function is from the `git` plugin. This function is
+# conflicting with the zsh-autosuggestions for navigating to Git worktrees,
+# making naviation to worktrees slightly annoying.
+unfunction work_in_progress # From the git plugin
