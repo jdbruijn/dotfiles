c2cd0197ab75280c4f0ba94099a1c6e57f6a2d59
Jeroen de Bruijn <vv9239@gmail.com>
2025-09-11 22:02:06 +0200

    feat(zsh): add aliases for docker, docker-desktop, git, isodate and vscode

---
+++ zsh/aliases/isodate.zsh
@@ -0,0 +1,10 @@
+# Currently a copy from Oh My Zsh.
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/isodate/isodate.plugin.zsh
+
+# work with date ISO 8601 easy
+
+alias isodate="date +%Y-%m-%dT%H:%M:%S%z"
+alias isodate_utc="date -u +%Y-%m-%dT%H:%M:%SZ"
+alias isodate_basic="date -u +%Y%m%dT%H%M%SZ"
+alias unixstamp="date +%s"
+alias date_locale="date +"%c""
