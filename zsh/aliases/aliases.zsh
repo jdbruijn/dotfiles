308bb2590851346077c8b6a09831b8b0d392982e
Jeroen de Bruijn <jeroen@withthegrid.com>
2025-09-20 14:49:34 +0200

    fix(zsh): add `bat` aliases

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -1,10 +1,3 @@
-# Command replacements
-alias cat='bat --paging=never'
-
-# Project folder navigation
-alias p='cd ~/projects'
-alias wp='cd ~/work/projects'
-
 # Short version to run npm scripts.
 alias nr='npm run --silent'
 
---
+++ zsh/aliases/bat.zsh
@@ -0,0 +1,3 @@
+alias cat='bat --paging=never'
+alias cata='bat --paging=never --show-all'
+alias catp='bat --paging=never --plain'
