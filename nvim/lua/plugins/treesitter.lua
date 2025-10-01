9ff31ca3df22c3910c4e199aca4a25ad981c1bb0
Jeroen de Bruijn <jeroen@withthegrid.com>
2025-10-01 15:41:18 +0200

    feat(neovim): add `nvim-treesitter-context` plugin to keep the class/method/... context on top

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,39 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-    vim.treesitter.language.register('markdown', 'mdx')
-  end,
-  opts = {
-    auto_install = true,
-    ensure_installed = {
-      'bash',
-      'c',
-      'cmake',
-      'cpp',
-      'dockerfile',
-      'editorconfig',
-      'go',
-      'html',
-      'javascript',
-      'lua',
-      'markdown_inline',
-      'markdown',
-      'rust',
-      'toml',
-      'typescript',
-      'vue',
-      'yaml',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+      vim.treesitter.language.register('markdown', 'mdx')
+    end,
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        'bash',
+        'c',
+        'cmake',
+        'cpp',
+        'dockerfile',
+        'editorconfig',
+        'go',
+        'html',
+        'javascript',
+        'lua',
+        'markdown_inline',
+        'markdown',
+        'rust',
+        'toml',
+        'typescript',
+        'vue',
+        'yaml',
+      },
+      highlight = {
+        additional_vim_regex_highlighting = false,
+        enable = true,
+      },
+      indent = { enable = true },
+      sync = false,
     },
-    highlight = {
-      additional_vim_regex_highlighting = false,
-      enable = true,
-    },
-    indent = { enable = true },
-    sync = false,
   },
+  { 'nvim-treesitter/nvim-treesitter-context' },
 }
