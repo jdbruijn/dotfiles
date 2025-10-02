f38a2d05eaf1fc85e1047a3d19ff07a5a29e01f5
Jeroen de Bruijn <jeroen@withthegrid.com>
2025-10-02 17:22:02 +0200

    feat(neovim): add `<leader>sF` keymapping to navigation for searching hidden files

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -57,6 +57,9 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.find_files({ hidden = true })
+      end, { desc = '[S]earch hidden [F]iles' })
       vim.keymap.set('n', '<leader>/', function()
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
           winblend = 10,
