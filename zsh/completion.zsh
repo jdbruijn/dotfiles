ab129f87d1f8839dd4c9fa945828288f88a15c97
Jeroen de Bruijn <jeroen@withthegrid.com>
2025-10-07 17:20:47 +0200

    fix(zsh): set case-insensitive completion

---
+++ zsh/completion.zsh
@@ -0,0 +1,6 @@
+# Configure the Zsh completion, see the following references for details on the
+# parameters and options used.
+# https://zsh.sourceforge.io/Doc/Release/Completion-System.html
+
+# Case-insensitive completion.
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
