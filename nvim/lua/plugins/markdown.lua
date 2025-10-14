c757bfe15685b0d5c53713bf284ae3d4f1417c17
Jeroen de Bruijn <vv9239@gmail.com>
2025-10-14 20:00:42 +0200

    fix(nvim): set Markdown syntax highlight language aliases for `shell` and `ts`

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,10 +1,14 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
+  'meanderingprogrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   opts = {
     completions = { lsp = { enabled = true } },
   },
   config = function(_, opts)
+    require('nvim-web-devicons').set_icon_by_filetype({
+      ts = 'ts',
+      shell = 'sh',
+    })
     require('render-markdown').setup(opts)
     vim.keymap.set('n', '<leader>p', '<CMD>RenderMarkdown toggle<CR>', { desc = 'Toggle Markdown [P]review' })
   end,
