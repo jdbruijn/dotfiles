4f8818be3a97547c26833b128ea904aa3d8ba91c
Jeroen de Bruijn <jeroen@withthegrid.com>
2026-03-05 20:51:29 +0100

    refactor(zsh): move `mcd` function to `cd` aliases file

--- zsh/aliases/cd.zsh
+++ zsh/aliases/cd.zsh
@@ -4,3 +4,11 @@ alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
 alias ......='cd ../../../../../'
+
+# Create a directory and change directories into it.
+#
+# Usage: mcd <path>
+function mcd() {
+  mkdir -p -- "${1}"
+  cd -- "${1}"
+}
