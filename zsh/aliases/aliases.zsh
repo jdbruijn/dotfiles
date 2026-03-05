5bc3b0b902dd74369ef6500eba5b2ce88c8a334e
Jeroen de Bruijn <jeroen@withthegrid.com>
2026-03-05 20:35:49 +0100

    feat(zsh): add `gclb` alias to create a bare `Git` clone

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -9,3 +9,9 @@ alias path='echo -e ${PATH//:/\\n}'
 
 # Git.
 alias glolnc='glol --grep="^chore" --invert-grep'
+
+function gclb() {
+  local repository="${1}"
+
+  git clone --recurse-submodules "${repository}" --bare --config remote.origin.fetch='+refs/heads/*:refs/remotes/origin/*'
+}
